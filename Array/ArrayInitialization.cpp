d1d5fa1550c93e89db8e32e858850e05fcec9704
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-01-09 21:07:32 +0530

    Update

--- Array/ArrayInitialization.cpp
+++ Array/ArrayInitialization.cpp
@@ -12,8 +12,12 @@ int main(){
     int size;
     cout<<"Enter the size: "<<endl;
     cin>>size;
+    int arr[size];
+    cout<<"Enter the element of the array: ";
     for(int i=0;i<size;i++){
         cin>>arr[i];
     }
-    PrintArray(arr[size],size);
+    PrintArray(arr, size);
+    return 0;
 }
+
