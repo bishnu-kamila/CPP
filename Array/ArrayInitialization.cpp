0659a6dcee49eaece7b88e4aaec5726f783408c0
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-01-09 17:16:28 +0530

    Update the code base

---
+++ Array/ArrayInitialization.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void PrintArray(int arr[], int size){
+    cout<<"The array is: "<<endl;
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+
+int main(){
+    int size;
+    cout<<"Enter the size: "<<endl;
+    cin>>size;
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    PrintArray(arr[size],size);
+}
