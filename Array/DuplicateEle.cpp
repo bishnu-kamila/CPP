945861412c6e324c375360a00a4f27c81b7ee26c
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-01-20 23:59:49 +0530

    Make new program find duplicate element present in an array

---
+++ Array/DuplicateEle.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void printArr(int[] arr, int size){
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+void findDuplicate(int[] arr, int size){
+    for(int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+            if(arr[i]==arr[j]){
+                cout<<"Duplicate element is "<<arr[i]<<endl;
+            }
+        }
+    }
+}
+int main(){
+    int size;
+    cout<<"Enter the size";
+    cin>>size;
+    int arr[size];
+    cout<<"Enter array elements: ";
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    printArr(arr, size);
+    findDuplicate(arr, size);
+}
