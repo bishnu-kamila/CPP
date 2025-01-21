872387a9ea3ed612e5e66279c6709d5bd64a83ac
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-01-21 22:52:46 +0530

    update

--- Array/DuplicateEle.cpp
+++ Array/DuplicateEle.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 using namespace std;
-void printArr(int[] arr, int size){
+void printArr(int arr[], int size){
     for(int i=0;i<size;i++){
         cout<<arr[i]<<" ";
     }
+    cout<<endl;
 }
-void findDuplicate(int[] arr, int size){
+void findDuplicate(int arr[], int size){
     for(int i=0;i<size;i++){
         for(int j=i+1;j<size;j++){
             if(arr[i]==arr[j]){
