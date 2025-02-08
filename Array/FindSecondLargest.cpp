5a3727ac4efbdb34b9d8918cf2f4643f47f96ea6
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-02-08 23:28:12 +0530

    update

--- Array/FindSecondLargest.cpp
+++ Array/FindSecondLargest.cpp
@@ -1,18 +1,16 @@
 #include<iostream>
 using namespace std;
 int FindSecLargestEle(int arr[], int size){
-    int largest = 0;
-    int secL = 0;
+    int largest = arr[0];
+    int secL = arr[0];
     for(int i=0;i<size;i++){
-        for(int j=i+1;j<size;j++){
-             if(arr[i]>arr[j]){
-            largest = arr[i];
-            secL = arr[j];
-        }
+            if(largest<arr[i]){
+                largest=arr[i];
+            }
         }
     }
-    return secL;
-}
+    
+
 int main(){
     int size;
     cout<<"Enter the size: "<<endl;
