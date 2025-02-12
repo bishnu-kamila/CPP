ad31efc2991949df75d7b56cc62dd8270885c841
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-02-12 18:18:29 +0530

    update

---
+++ Array/AddOne.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int size;
+    cout<<"Enter the size: "<<endl;
+    cin>>size;
+    int arr[size];
+    cout<<"Enter the array elements: "<<endl;
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+}
