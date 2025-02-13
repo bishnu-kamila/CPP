31d1c207331379d95ece8d40ee4aa477683086e0
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-02-13 17:52:47 +0530

    update

--- Array/AddOne.cpp
+++ Array/AddOne.cpp
@@ -5,7 +5,12 @@ int addOne(int arr[], int size){
         arr[i]+=1;
     }
 }
-
+void printArray(int arr[], int size){
+    cout<<"The array is: "<<endl;
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
 int main(){
     int size;
     cout<<"Enter the size: "<<endl;
@@ -15,5 +20,7 @@ int main(){
     for(int i=0;i<size;i++){
         cin>>arr[i];
     }
+    printArray(arr,size);
     addOne(arr,size);
+    printArray(arr,size);
 }
