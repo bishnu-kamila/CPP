620eb0cafa2cf3afe19171944ae91c375d5acb69
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-02-14 23:53:39 +0530

    update

---
+++ Array/MergeSort.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int MergSort(int arr[], int size){
+    
+}
+int main(){
+    int size;
+    cout<<"Enter the size : "<<endl;
+    cin>>size;
+    int arr[size];
+    cout<<"Enter the elements of the array: ";
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+}
