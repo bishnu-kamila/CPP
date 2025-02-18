711c3dd24b0d119c8ab136916b94f71042d608f1
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-02-18 23:51:25 +0530

    update

--- Array/BinarySearch.cpp
+++ Array/BinarySearch.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
-
+int binarySearch(int arr[],int low, int high, int x){
+    while(low<=high){
+        
+    }
+}
 int main(){
     int size;
     cout<<"Enter the size of the array: ";
