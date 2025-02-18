6e9ab6fa2f813dc215bb3f3f58be495ee8b6dbe7
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-02-18 22:53:51 +0530

    update

--- Array/BinarySearch.cpp
+++ Array/BinarySearch.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 using namespace std;
-int main(){
 
+int main(){
+    int size;
+    cout<<"Enter the size of the array: ";
+    cin>>size;
+    int arr[size];
+    cout<<"Enter the array elements: ";
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
 }
