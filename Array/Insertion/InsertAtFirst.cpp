cb67d3677474d6b10fed2475ba29064a2cea80ea
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-02-19 17:18:06 +0530

    update

---
+++ Array/Insertion/InsertAtFirst.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void printArr(int arr[], int size){
+    cout<<"The array is : "<<endl;
+    for(int i=0;i<size;i++){
+        cout<<arr[i];
+    }
+}
+int main(){
+    int arr[100] = {1,2,3,48};
+    int size = 4;
+
+}
