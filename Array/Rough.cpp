ecb056d34f18d883990dfc3c2cbcbd03ff3cf960
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-02-21 17:21:21 +0530

    update

--- Array/Rough.cpp
+++ Array/Rough.cpp
@@ -5,24 +5,23 @@ void printArray(int arr[], int size){
     for(int i=0;i<size;i++){
         cout<<arr[i]<<" ";
     }
-}
-int addOne(int arr[], int size){
-    for(int i=size;i>0;i--){
-        cout<<arr[i];
-    }
+    cout<<endl;
 }
 int main(){
-    int size;
-    cout<<"Enter the size: "<<endl;
-    cin>>size;
-    int arr[size];
-    cout<<"Enter the array elements: "<<endl;
-    for(int i=0;i<size;i++){
+    int arr[100], n,x;
+    cout<<"Enter the size of the array: "<<endl;
+    cin>>n;
+    cout<<"Enter the element of the array: ";
+    for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    printArray(arr,size);
-    cout<<endl;
-    addOne(arr,size);
-    cout<<endl;
-    printArray(arr,size);
+    printArray(arr,n);
+    cout<<"Enter the element which we want to insert: "<<endl;
+    cin>>x;
+    for(int i=n;i>=3;i--){
+        arr[i]=arr[i-1];
+    }
+    arr[3]= x;
+    n++;
+    printArray(arr,n);
 }
