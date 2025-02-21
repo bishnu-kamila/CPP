8022a4146822a2a2bbb3fea1876b70dbb9974a72
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-02-21 22:59:50 +0530

    update

---
+++ Array/Deletion/DeleteAtEnd.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void printArray(int arr[], int size){
+    cout<<"The array is : "<<endl;
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    int size;
+    cout<<"Enter the size: "<<endl;
+    cin>>size;
+    int arr[size];
+    cout<<"Enter the array elements: "<<endl;
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    printArray(arr,size);
+    int p;
+    cout<<"Enter the index which we want to delete"<<endl;
+    cin>>p;
+    for(int i=p;i<size-1;i++){
+        arr[i]=arr[i+1];
+    }
+    size--;
+    printArray(arr,size);
+}
