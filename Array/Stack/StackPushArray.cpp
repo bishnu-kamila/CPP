6fdf5554e71941d9def0467d996878e72425e5cd
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-03-04 23:47:45 +0530

    update

--- Array/Stack/StackPushArray.cpp
+++ Array/Stack/StackPushArray.cpp
@@ -4,13 +4,13 @@ void push(){
 
 }
 void pop(){
-    
+
 }
 int main(){
     int size;
     cout<<"Enter the size"<<endl;
     cin>>size;
-    int arr[size];
+    int *arr[size];
     cout<<"Enter the array elements: "<<endl;
     for(int i=0;i<size;i++){
         cin>>arr[i];
