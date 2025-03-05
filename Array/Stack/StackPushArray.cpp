488cc0b6dfd6c93e393fb3d08f27dc55c1ce40df
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-03-05 18:28:08 +0530

    update

--- Array/Stack/StackPushArray.cpp
+++ Array/Stack/StackPushArray.cpp
@@ -1,11 +1,39 @@
 #include<iostream>
 using namespace std;
-void push(){
+class stack{
+    int* arr;
+    int top;
 
-}
-void pop(){
+    public:
+    stack(){
+        arr = new int[n];
+        top = -1;
+    }
+    void push(int x){
+        if(top==n-1){
+            cout<<"Stack overflow"<<endl;
+            return;
+        }
+        top++;
+        arr[top] = x;
+    }
+    void pop(){
+        if(top==-1){
+            cout<<"No element to pop"<<endl;
+            return;
+        }
+        top--;
+    }
+    int top(){
+        if(top==-1){
+            cout<<"No element in stack"<<endl;
+            return;
+        }
+        return arr[top];
+    }
+};
 
-}
+
 int main(){
     int size;
     cout<<"Enter the size"<<endl;
