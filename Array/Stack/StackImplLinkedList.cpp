46110c8b003b2b190ac1e80d13b1265d0d29f036
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-03-06 12:31:17 +0530

    update

--- Array/Stack/StackImplLinkedList.cpp
+++ Array/Stack/StackImplLinkedList.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 using namespace srd;
+struct Node{
+    int data;
+    Node* next;
+};
 class stack{
-    
+private:
+Node* top;
+public:
+Stack(){
+    top = nullptr;
+}
 }
 int main(){
 
