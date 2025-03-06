314c2a769fc8dbc982ff33c34c103fc53d50c74f
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-03-06 12:36:30 +0530

    update

--- Array/Stack/StackImplLinkedList.cpp
+++ Array/Stack/StackImplLinkedList.cpp
@@ -11,7 +11,60 @@ public:
 Stack(){
     top = nullptr;
 }
+void push(int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = top;
+    top = newNode;
+    cout << value << " pushed onto stack." << endl;
 }
+void pop() {
+    if (isEmpty()) {
+        cout << "Stack underflow!" << endl;
+        return;
+    }
+    Node* temp = top;
+    top = top->next;
+    cout << temp->data << " popped from stack." << endl;
+    delete temp;
+}
+int peek() {
+    if (isEmpty()) {
+        cout << "Stack is empty!" << endl;
+        return -1;
+    }
+    return top->data;
+}
+bool isEmpty() {
+    return top == nullptr;
+}
+void display() {
+        if (isEmpty()) {
+            cout << "Stack is empty!" << endl;
+            return;
+        }
+        Node* temp = top;
+        cout << "Stack elements: ";
+        while (temp) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+    ~Stack() {
+        while (!isEmpty()) {
+            pop();
+        }
+    }
+};
 int main(){
-
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.display();
+    cout << "Top element: " << s.peek() << endl;
+    s.pop();
+    s.display();
+    return 0;
 }
