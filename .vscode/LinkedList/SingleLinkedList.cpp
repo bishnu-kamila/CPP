8c6f43318d6e9f50f6211dc14ba1ef8bca0951f9
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-03-15 16:52:01 +0530

    update

--- .vscode/LinkedList/SingleLinkedList.cpp
+++ .vscode/LinkedList/SingleLinkedList.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
 using namespace std;
-class Node[
-    int node;
-]
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    Node(){
+        data = 0;
+        next = NULL;
+    }
+    Node(int data){
+        
+    }
+}
+
 int main(){
     
 }
