faaa58a18865119c753c5429c197a0e4bac5e5a8
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-03-15 18:09:30 +0530

    update

--- .vscode/LinkedList/SingleLinkedList.cpp
+++ .vscode/LinkedList/SingleLinkedList.cpp
@@ -21,8 +21,36 @@ class LinkedList{
     LinkedList(){
         head = NULL;
     }
-}
+    void insertAtHead(int data){
+        Node *newNode = new Node(data);
+        if(head == NULL){
+            head = newNode;
+            return;
+        }
+        newNode->next = this->head;
+        this->head = newNode;
+    }
+
+    void print(){
+        Node *temp = head;
+        if(head == NULL){
+            cout<<"List empty"<<" ";
+            temp = temp->next;
+        }
+    }
+};
 
 int main(){
-    
+    LinkedList list;
+
+    list.insertAtHead(4);
+    list.insertAtHead(3);
+    list.insertAtHead(2);
+    list.insertAtHead(1);
+
+    cout<<"Element of the list are: ";
+
+    list.print();
+    cout<<endl;
+    return 0;
 }
