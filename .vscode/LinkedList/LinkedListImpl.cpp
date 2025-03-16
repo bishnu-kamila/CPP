cd2e70cd89f66485d37047f11c3450debfff26b4
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-03-16 14:10:53 +0530

    update

--- .vscode/LinkedList/LinkedListImpl.cpp
+++ .vscode/LinkedList/LinkedListImpl.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
 using namespace std;
-class Node[
-    
-]
+class Node{
+    int data;
+    Node *next;
+
+    Node(){
+        data = 0;
+        next = NULL;
+    }
+
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+}
 int main(){
     
 }
