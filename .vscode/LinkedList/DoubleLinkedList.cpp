f47c770c3e03ea2aea59d9526087f079b737b597
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-03-20 18:21:59 +0530

    update

--- .vscode/LinkedList/DoubleLinkedList.cpp
+++ .vscode/LinkedList/DoubleLinkedList.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
 using namespace std;
+struct Node {
+    int data;
+    Node* prev;
+    Node* next;
+
+    Node(int value) : data(value), prev(nullptr), next(nullptr) {}
+};
+
+class DoublyLinkedList {
+    private:
+        Node* head;
+        Node* tail;
+
 int main(){
     
 }
