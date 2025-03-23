6ace0f09cf05529f3be698f30334489dc5b6804b
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-03-23 23:45:34 +0530

    update

--- Array/ArrayList.cpp
+++ Array/ArrayList.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 int main(){
     ArrayList<Integer> list = new ArrayList<Integer>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
     
+    cout<<list;
 }
