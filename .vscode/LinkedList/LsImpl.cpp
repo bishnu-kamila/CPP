91491d5ecce0141c89626cec38e1e5bdc2428edd
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-03-28 16:58:49 +0530

    update

---
+++ .vscode/LinkedList/LsImpl.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data
+    Node *next;
+
+    Node(){
+        data = 0;
+        next = NULL;
+    }
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+class LinkedList{
+    
+}
