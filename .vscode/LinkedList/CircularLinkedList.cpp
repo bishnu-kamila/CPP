c67c4211195109be7ba4eb56a102403b569b3824
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-04-04 18:03:36 +0530

    update

--- .vscode/LinkedList/CircularLinkedList.cpp
+++ .vscode/LinkedList/CircularLinkedList.cpp
@@ -3,6 +3,12 @@ using namespace std;
 class Node{
     int data;
     Node *next;
+    Node *prev;
+    Node(){
+        this->data = data;
+        this->prev = NULL;
+        this->next = NULL;
+    }
 }
 class LinkedList{
     
