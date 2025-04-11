0c48b26ae7bac0e8ea72579dee445a4dbb23f4c4
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-04-11 15:54:36 +0530

    update

---
+++ .vscode/LinkedList/DeleteAtbegin.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(){
+        data=0;
+        next = NULL;
+    }
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+class LinkedList{
+    private:
+    Node *head;
+
+    public:
+    LinkedList(){
+        head = NULL;
+    }
+
+    void insertAtEnd(int data){
+        Node *newNode = new Node(data);
+        if(head == NULL){
+            head = newNode;
+            return;
+        }
+        Node *temp = head;
+        while(temp->next !=NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    void deleteAtHead(){
+        Node* toDelete = head;
+        head = head->next;
+        delete toDelete;
+    }
+
+    void deleteAny(int data){
+        Node* temp = head;
+        while(temp->next->data!=data){
+            temp=temp->next;
+        }
+        Node* todelete = temp->next;
+        temp->next = temp->next->next;
+
+        delete todelete;
+    }
+
+    void print(){
+        Node *temp = head;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+        cout<<endl;
+    }
+};
+int main(){
+    LinkedList list;
+    list.insertAtEnd(10);
+    list.insertAtEnd(101);
+    list.insertAtEnd(102);
+    list.insertAtEnd(103);
+    list.insertAtEnd(105);
+    list.insertAtEnd(107);
+    list.insertAtEnd(1001);
+
+    list.print();
+    cout<<"After delete head"<<endl;
+    list.deleteAtHead();
+    list.print();
+    list.deleteAny(105);
+    cout<<"after delete Node at any position"<<endl;
+    list.print();
+}
