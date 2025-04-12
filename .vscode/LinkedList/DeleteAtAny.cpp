201b146f0b5863b01fdceec49fd5103c3319cd70
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-04-12 23:09:50 +0530

    update

---
+++ .vscode/LinkedList/DeleteAtAny.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Node{
+    int data;
+    Node *next;
+
+    Node(){
+        data = 0;
+        next = NULL;
+    }
+    NOde(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+class LinkedList{
+    
+}
+int main(){
+
+}
