f47c54233fc10273974313ce812378571d599997
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-04-13 22:59:03 +0530

    update

--- .vscode/LinkedList/DeleteAtAny.cpp
+++ .vscode/LinkedList/DeleteAtAny.cpp
@@ -14,11 +14,55 @@ class Node{
     }
 };
 class LinkedList{
+    private:
     Node *head;
+    public:
     LinkedList(){
-        
+        head = NULL;
+    }
+
+    void addNode(int data){
+        Node* newNode = new Node(data);
+        if(head == NULL){
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    void deleteAtAnyPosition(int data){
+        Node* temp = head;
+        while(temp->next->data != NULL){
+            temp = temp->next;
+        }
+        Node* toDelete = head;
+        temp->next = temp->next->next;
+
+        delete toDelete;
+    }
+
+    void print(){
+        Node* temp = head;
+        while(temp!= NULL){
+            cout<<temp->data<<" ";
+            temp= temp->next;
+        }
+        cout<<endl;
     }
 }
 int main(){
+    LinkedList list;
+    list.addNode(10);
+    list.addNode(11);
+    list.addNode(12);
+    list.addNode(13);
+    list.addNode(14);
 
+    cout<<"After Delete Node"<<endl;
+
+    list.deleteAtAnyPosition(13);
 }
