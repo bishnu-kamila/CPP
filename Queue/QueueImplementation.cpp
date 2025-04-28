c867d4eb0eb2d3ca6a0f86fe0289d306223cd3fd
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-04-28 17:46:48 +0530

    update

--- Queue/QueueImplementation.cpp
+++ Queue/QueueImplementation.cpp
@@ -1,17 +1,85 @@
 #include<iostream>
 using namespace std;
-class Queue{
-    int item[size];
-    int front,rear;
-
-    Queue(){
-        front = -1;
-        rear = -1;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(){
+        data = 0;
+        next = NULL;
     }
-    void enqueue(){
-        
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
     }
 };
+class Queue{
+    private:
+    Node* front;
+    Node* rear;
+public:
+    Queue(){
+        front = NULL;
+        rear = NULL;
+    }
+    void enqueue(int ele){
+        Node* newNode = new Node();
+        newNode->data = ele;
+        newNode->next = NULL;
+
+        if(rear == NULL){
+            front = rear = newNode;
+            return;
+        }
+
+        rear->next = newNode;
+        rear = newNode;
+
+    }
+    void dequeue(){
+        if(front == NULL){
+            cout<<"Queue is empty"<<endl;
+            rear = NULL;
+        }
+
+        Node* temp = front;
+        front = front->next;
+
+        // if(front == NULL){
+        //     rear = NULL;
+        // }
+
+        delete temp;
+
+    }
+
+    void display(){
+        if(front == NULL){
+            cout<<"Queue is empty"<<endl;
+        }
+        Node* temp = front;
+        while(temp!=NULL){
+            cout<<temp->data << " ";
+            temp = temp->next;
+        }
+        cout<<endl;
+    }
+};
+
 int main(){
+    Queue qqq;
+    qqq.enqueue(10); // front
+    qqq.enqueue(100);
+    qqq.enqueue(101);
+    qqq.enqueue(102);
+    qqq.enqueue(103);
+    qqq.enqueue(104); // rear
+    
+    cout<<"Queue elements are"<<endl;
+    qqq.display();
+    qqq.dequeue();
+    cout<<"After dequeue the Queue elements are"<<endl;
+    qqq.display();
+
 
 }
